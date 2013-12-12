ac87d68a2d8e268fe8b186fafcaf14aa35ef6a86
Carlos Figueira <carlosfigueirafilho@hotmail.com>
2013-12-12 11:23:28 -0800

    Code for the post at http://blogs.msdn.com/b/carlosfigueira/archive/2013/12/12/expanded-login-scopes-in-azure-mobile-services.aspx

---
+++ AzureMobileServices/GettingUserInfoFromAuthProviders/GettingUserInfoFromAuthProviders/MainPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace GettingUserInfoFromAuthProviders
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public static MobileServiceClient MobileService = new MobileServiceClient(
+            "https://YOUR-SERVICE-NAME.azure-mobile.net/",
+            "YOUR-SERVICE-APP-KEY"
+        );
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private async void btnFacebook_Click(object sender, RoutedEventArgs e)
+        {
+            await Login(MobileServiceAuthenticationProvider.Facebook);
+        }
+
+        private async void btnGoogle_Click(object sender, RoutedEventArgs e)
+        {
+            await Login(MobileServiceAuthenticationProvider.Google);
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            MobileService.Logout();
+        }
+
+        private async void btnTwitter_Click(object sender, RoutedEventArgs e)
+        {
+            await Login(MobileServiceAuthenticationProvider.Twitter);
+        }
+
+        private async void btnMicrosoft_Click(object sender, RoutedEventArgs e)
+        {
+            await Login(MobileServiceAuthenticationProvider.MicrosoftAccount);
+        }
+
+        private async Task Login(MobileServiceAuthenticationProvider provider)
+        {
+            try
+            {
+                var user = await MobileService.LoginAsync(provider);
+                AddToDebug("Logged in as {0}", user.UserId);
+            }
+            catch (Exception ex)
+            {
+                this.AddToDebug("Error: {0}", ex);
+            }
+        }
+
+        private async void btnCallAPI_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var result = await MobileService.InvokeApiAsync("userinfo", HttpMethod.Get, null);
+                this.AddToDebug("Result: {0}", result);
+            }
+            catch (Exception ex)
+            {
+                this.AddToDebug("Error: {0}", ex);
+            }
+        }
+
+        private void AddToDebug(string text, params object[] args)
+        {
+            if (args != null && args.Length > 0) text = string.Format(text, args);
+            this.txtDebug.Text = this.txtDebug.Text + text + Environment.NewLine;
+        }
+    }
+}
---
+++ AzureMobileServices/GettingUserInfoFromAuthProviders/GettingUserInfoFromAuthProviders/ServerScripts/api/userInfo.js
@@ -0,0 +1,66 @@
+﻿exports.get = function (request, response) {
+    var user = request.user;
+    user.getIdentities({
+        success: function (identities) {
+            var req = require('request');
+            var url = null;
+            var oauth = null;
+            var userId = user.userId.split(':')[1];
+            console.log('Identities: ', identities);
+            if (identities.facebook) {
+                url = 'https://graph.facebook.com/me?access_token=' +
+                    identities.facebook.accessToken;
+            } else if (identities.google) {
+                url = 'https://www.googleapis.com/oauth2/v3/userinfo' +
+                    '?access_token=' + identities.google.accessToken;
+            } else if (identities.microsoft) {
+                url = 'https://apis.live.net/v5.0/me?access_token=' +
+                    identities.microsoft.accessToken;
+            } else if (identities.twitter) {
+                var consumerKey = process.env.MS_TwitterConsumerKey;
+                var consumerSecret = process.env.MS_TwitterConsumerSecret;
+                oauth = {
+                    consumer_key: consumerKey,
+                    consumer_secret: consumerSecret,
+                    token: identities.twitter.accessToken,
+                    token_secret: identities.twitter.accessTokenSecret
+                };
+                url = 'https://api.twitter.com/1.1/users/show.json?' +
+                    'user_id=' + userId + '&include_entities=false';
+            } else {
+                response.send(500, { error: 'No known identities' });
+                return;
+            }
+
+            if (url) {
+                var reqParams = { uri: url, headers: { Accept: 'application/json' } };
+                if (oauth) {
+                    reqParams.oauth = oauth;
+                }
+                req.get(reqParams, function (err, resp, body) {
+                    if (err) {
+                        console.error('Error calling provider: ', err);
+                        response.send(500, { error: 'Error calling provider' });
+                        return;
+                    }
+
+                    if (resp.statusCode !== 200) {
+                        console.error('Provider call did not return success: ', resp.statusCode);
+                        response.send(500, { error: 'Provider call did not return success: ' + resp.statusCode });
+                        return;
+                    }
+
+                    try {
+                        var userData = JSON.parse(body);
+                        response.send(200, userData);
+                    } catch (ex) {
+                        console.error('Error parsing response: ', ex);
+                        response.send(500, { error: ex });
+                    }
+                });
+            } else {
+                response.send(500, { error: 'Not implemented yet', env: process.env });
+            }
+        }
+    });
+};
